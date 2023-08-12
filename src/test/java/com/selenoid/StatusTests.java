079a299ee43bc9264536e713aa5dce075a746a2d
Marena <fabriana.sands@gmail.com>
2023-08-12 14:28:25 +0500

    first commit

---
+++ src/test/java/com/selenoid/StatusTests.java
@@ -0,0 +1,89 @@
+package com.selenoid;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+
+public class StatusTests {
+
+    /*
+    1. Make request to https://selenoid.autotests.cloud/status
+    2. Get response { total: 20, used: 0, queued: 0, pending: 0, browsers: ...
+    3. Check total is 20
+     */
+
+    @Test
+    void checkTotalMini() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotal() {
+        given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithLogs() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithSomeLogs() {
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithStatus() {
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithChrome() {
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20))
+                .body("browsers.chrome", hasKey("100.0"));
+    }
+}
